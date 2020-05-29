7100f147011ed8438c4c715d1f457991d1b45c7b
Nick Mathewson <nickm@torproject.org>
2020-05-29 13:43:23 -0400

    tell render script to number my sections.

--- tools/render.sh
+++ tools/render.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pandoc --toc --standalone \
+       --number-sections \
        --metadata pagetitle="Walking Onions Specification" \
        --from markdown specs/*.md > rendered.html
 
